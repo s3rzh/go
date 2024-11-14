b429d0fce8b34881459063cc0a8dfc47e05fcba3
s3rzh <serzh.vs@gmail.com>
2024-11-14 15:46:19 +0300

    Create main.go

---
+++ concurrency_tasks/06_select/01_buf_chan_read_write/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	bufChan := make(chan string, 1)
+	bufChan <- "first"
+
+  // тк в буферезированом канале уже есть запись, то будет исполнен однозначно case str := <-bufChan: тк записать уже некуда
+	select {
+	case str := <-bufChan:
+		fmt.Println("read", str)
+	case bufChan <- "second":
+		fmt.Println("write", <-bufChan, bufChan)
+	}
+}
