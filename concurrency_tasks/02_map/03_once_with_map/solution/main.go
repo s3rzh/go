56b7ddf52a698fd6576a5d47f1c2115febf07614
s3rzh <serzh.vs@gmail.com>
2024-11-14 15:08:48 +0300

    Update main.go

--- concurrency_tasks/02_map/03_once_with_map/solution/main.go
+++ concurrency_tasks/02_map/03_once_with_map/solution/main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	// 3, 4, 5, 0, 4, 9, 8, 6, 6, 5, 5, 4, 4, 4, 2, 1, 2, 3, 1 ...
 
-	uniqueIDs := make(chan int, capacity)
+	uniqueIDs := make(chan int, capacity) // канал для уникальных значений
 	wg := &sync.WaitGroup{}
 
 	for i := 0; i < capacity; i++ {
@@ -27,7 +27,7 @@ func main() {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			mu.Lock()
+			mu.Lock() // ставим блокировку и для чтения (32) и для записи (33)
 			defer mu.Unlock()
 			if _, ok := alreadyStored[doubles[i]]; !ok {
 				alreadyStored[doubles[i]] = struct{}{}
@@ -38,11 +38,11 @@ func main() {
 	}
 
 	wg.Wait()
-	close(uniqueIDs)
+	close(uniqueIDs) // не забываем закрыть канал, чтобы for range ниже завершился
 	for val := range uniqueIDs {
 		fmt.Println(val)
 	}
 	// close(uniqueIDs) // здесь неправильно
-	fmt.Printf("len of ids: %d\n", len(uniqueIDs)) // 0, 1, 2, 3, 4 ...
+	fmt.Printf("len of ids: %d\n", len(uniqueIDs)) // 0
 	fmt.Println(uniqueIDs)
 }
