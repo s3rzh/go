64d56531eedb70ab722d017bee5b348d0b1e0ac1
s3rzh <serzh.vs@gmail.com>
2024-11-14 10:18:08 +0300

    Create main.go

---
+++ concurrency_tasks/02_map/02_reads_writes/solution/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// find&fix bug with reads-writes
+func main() {
+	storage := make(map[int]int, 1000)
+
+	wg := sync.WaitGroup{}
+	reads := 1000
+	writes := 1000
+	mu := sync.RWMutex{}
+
+	wg.Add(writes)
+	for i := 0; i < writes; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+
+			mu.Lock()
+			defer mu.Unlock()
+			storage[i] = i
+		}()
+	}
+	wg.Add(reads)
+	for i := 0; i < reads; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+
+			mu.RLock()
+			defer mu.RUnlock()
+			_, _ = storage[i]
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(storage)
+}
