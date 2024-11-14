756d30ab792539accecb40d719d4077287ff4968
s3rzh <serzh.vs@gmail.com>
2024-11-14 16:46:05 +0300

    Update main.go

--- concurrency_tasks/06_select/02_ctx_with_timeout/main.go
+++ concurrency_tasks/06_select/02_ctx_with_timeout/main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// add timeout to avoid long waiting
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	chanForResp := make(chan int)
+	go RPCCall(chanForResp)
+
+	result := <-chanForResp
+	fmt.Println(result)
+}
+
+func RPCCall(ch chan<- int) {
+	// sleep 0-4 sec
+	time.Sleep(time.Second * time.Duration(rand.Intn(5)))
+
+	ch <- rand.Int()
+}
