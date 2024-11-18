48b20ab8ce7083e065528de346f3e2535155b97a
s3rzh <serzh.vs@gmail.com>
2024-11-18 16:01:24 +0300

    Create main.go

---
+++ concurrency_tasks/06_select/06_default_or_context/solution/2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// avoid deadlock
+func main() {
+	ch := make(chan int)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
+  
+	go func() {
+		select {
+		case val := <-ch:
+			fmt.Println(val)
+		case <-ctx.Done():
+			fmt.Println("Done")
+		}
+	}()
+	ch <- 42 // или cancel()
+	time.Sleep(time.Second)
+	fmt.Println(runtime.NumGoroutine())
+}
+
+// здесь мы добавили контекст
+// и записали значение в канал
+
+// по сути можно делать что-то одно
+// отменять контекст
+// или
+// записывать значение в канал
