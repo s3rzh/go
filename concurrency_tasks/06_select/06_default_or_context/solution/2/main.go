755317de97f411b01907545aab430f3dcad5c920
s3rzh <serzh.vs@gmail.com>
2024-11-18 16:02:57 +0300

    Update main.go

--- concurrency_tasks/06_select/06_default_or_context/solution/2/main.go
+++ concurrency_tasks/06_select/06_default_or_context/solution/2/main.go
@@ -12,7 +12,7 @@ func main() {
 	ch := make(chan int)
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
-  	defer cancel()
+  	defer cancel() // на всякий случай, чтобы утечки зависшей горутине не было
   
 	go func() {
 		select {
@@ -24,7 +24,7 @@ func main() {
 	}()
 	ch <- 42 // или cancel()
 	time.Sleep(time.Second)
-	fmt.Println(runtime.NumGoroutine())
+	fmt.Println(runtime.NumGoroutine()) // показываем кол-во активных горутин в момент этого вызова
 }
 
 // здесь мы добавили контекст
