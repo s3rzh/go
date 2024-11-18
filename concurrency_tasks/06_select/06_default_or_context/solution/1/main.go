dc38c0d58d0fef172e24dc1f8241ee5c17c055aa
s3rzh <serzh.vs@gmail.com>
2024-11-18 15:59:36 +0300

    Create main.go

---
+++ concurrency_tasks/06_select/06_default_or_context/solution/1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		select {
+		case val := <-ch:
+			fmt.Println(val)
+		default:                  
+			fmt.Println("default")
+		}
+	}()
+	time.Sleep(time.Second)
+	fmt.Println(runtime.NumGoroutine()) // позыкаем кол-во активных горутин в момент этого вызова
+}
+
+// решение - добавление default сценария
+
+// добавление горутины, чтобы увидеть количество активных горутин
+// при этом, если мы уберем default на 17 и 18 строке, у нас произойдет утечка горутины
+// main завершит свою работу, а созданная нами горутина нет
+
+// также можно решить задачу, используя контекст (см 2)
