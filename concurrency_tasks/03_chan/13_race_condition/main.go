1bad41c0fd7370a0972d1ebdafc9294cade0b61e
s3rzh <serzh.vs@gmail.com>
2024-11-18 19:18:16 +0300

    Update main.go

--- concurrency_tasks/03_chan/13_race_condition/main.go
+++ concurrency_tasks/03_chan/13_race_condition/main.go
@@ -9,18 +9,19 @@ var i int // i == 0
 
 // goroutine increment global variable i
 func worker(wg *sync.WaitGroup, ch chan int) {
-    i = <-ch 
-	  i = i + 1
+	i = <-ch
+	i = i + 1
 	wg.Done()
 }
+
 // попробовать с каналами
 func main() {
 	var wg sync.WaitGroup
-  ch := make(chan int)
+	ch := make(chan int)
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
-    go worker(&wg, ch)
-    ch <- i
+		go worker(&wg, ch)
+		ch <- i
 	}
 
 	// wait until all 1000 goroutines are done
