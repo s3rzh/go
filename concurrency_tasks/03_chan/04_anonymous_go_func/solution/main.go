45c8d4d7999b195b89ec1edcd43ad2f2561fae04
s3rzh <serzh.vs@gmail.com>
2024-11-18 15:03:06 +0300

    Update main.go

--- concurrency_tasks/03_chan/04_anonymous_go_func/solution/main.go
+++ concurrency_tasks/03_chan/04_anonymous_go_func/solution/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("main() started")
 	c := make(chan string)
-
+	var num = 12
 	// launch anonymous goroutine
 	go func(c chan string) {
 		fmt.Println("Hello " + <-c + "!")
