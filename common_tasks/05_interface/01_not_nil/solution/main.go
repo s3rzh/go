ef399f11a9d6e7528d025db7af34a8986314efb9
s3rzh <serzh.vs@gmail.com>
2024-11-19 19:33:50 +0300

    Create main.go

---
+++ common_tasks/05_interface/01_not_nil/solution/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type I interface {
+	Foo()
+}
+
+type S struct{}
+
+func (s *S) Foo() {
+	fmt.Println("Foo")
+}
+
+func Build() I {
+	// return &S{}
+	var res *S
+	return res
+}
+
+func main() {
+	i := Build()
+	if i != nil {
+		i.Foo()
+	} else {
+		fmt.Println("nil")
+	}
+}
+
+// в обоих случаях распечатается Foo
