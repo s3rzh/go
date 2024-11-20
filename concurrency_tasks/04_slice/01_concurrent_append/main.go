184528d286061a4f4199ab5dbb7606d9432a546a
s3rzh <serzh.vs@gmail.com>
2024-11-20 15:22:51 +0300

    Create main.go

---
+++ concurrency_tasks/04_slice/01_concurrent_append/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// what is the problem here?
+
+func main() {
+	input := []int{1, 2, 3, 4, 5}
+
+	wg := sync.WaitGroup{}
+	result := []int{}
+
+	for _, num := range input {
+		wg.Add(1)
+		go func(num int) {
+			defer wg.Done()
+			time.Sleep(150 * time.Millisecond)
+			data := num * 2
+			result = append(result, data)
+
+		}(num)
+	}
+
+	wg.Wait()
+
+	fmt.Println(result)
+}
+
+// here DATA RACE
