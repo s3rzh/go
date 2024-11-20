89f86b7732267eb35456add4ccf236488831c4bf
s3rzh <serzh.vs@gmail.com>
2024-11-20 18:27:15 +0300

    Update main.go

--- common_tasks/02_slice/01_print/main.go
+++ common_tasks/02_slice/01_print/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	slice := make([]int, 0, 1000)
 	slice = append(slice, 1, 2, 3)
-	fmt.Println(slice)     // ???
+	fmt.Println(slice)     // 1 2 3
 	process(slice)
-	fmt.Println(slice)     // ???
-	fmt.Println(slice[:6]) // ???
+	fmt.Println(slice)     // 3 нуля
+	fmt.Println(slice[:6]) // 6 нулей (последний не включительно)
 }
 
 func process(slice []int) {
