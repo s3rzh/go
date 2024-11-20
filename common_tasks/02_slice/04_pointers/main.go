3eaea5a9e9820d2e7a3bd1cb75441c86ac9304c9
s3rzh <serzh.vs@gmail.com>
2024-11-20 19:47:32 +0300

    Create main.go

---
+++ common_tasks/02_slice/04_pointers/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// что выведет программа и почему?
+func main() {
+	first := []int{10, 20, 30, 40}
+	second := make([]*int, len(first))
+	for i, v := range first {
+		second[i] = &v
+	}
+	fmt.Println(*second[0], *second[1]) // 10, 20
+}
