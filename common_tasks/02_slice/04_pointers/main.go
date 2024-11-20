a0690530b498cce0634095d87d3b186e59dbc813
s3rzh <serzh.vs@gmail.com>
2024-11-20 19:48:47 +0300

    Update main.go

--- common_tasks/02_slice/04_pointers/main.go
+++ common_tasks/02_slice/04_pointers/main.go
@@ -9,6 +9,7 @@ func main() {
 	first := []int{10, 20, 30, 40}
 	second := make([]*int, len(first))
 	for i, v := range first {
+		// v := v // до версии go 1.22
 		second[i] = &v
 	}
 	fmt.Println(*second[0], *second[1]) // 10, 20
