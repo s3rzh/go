c40aed7aa161ffb3d850739632c98580bdb6a3d9
s3rzh <serzh.vs@gmail.com>
2024-11-21 19:48:45 +0300

    Create main.go

---
+++ concurrency_tasks/03_chan/24_sequential_calls/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+// что выведет 3 или 6 и как исправить?
+
+func main() {
+	timeStart := time.Now()
+
+	_, _ = <-worker(), <-worker()
+
+	println(int(time.Since(timeStart).Seconds())) // 6 sec.
+}
+
+func worker() chan int {
+	ch := make(chan int)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		ch <- 42
+	}()
+
+	return ch
+}
+
+// Output: 6 sec.
