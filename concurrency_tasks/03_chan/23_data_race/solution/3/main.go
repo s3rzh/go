1a6ca6364b9f29641af9090f32202636d796e3a0
s3rzh <serzh.vs@gmail.com>
2024-11-21 18:24:58 +0300

    Create main.go

---
+++ concurrency_tasks/03_chan/23_data_race/solution/3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync/atomic"
+)
+
+func main() {
+
+	var done atomic.Bool //  The zero value is false.
+	runtime.GOMAXPROCS(1)
+
+	go func() {
+		done.Store(true)
+	}()
+
+	for done.Load() {
+	}
+	fmt.Println("finished")
+}
+
+// те на атамиках, без блокировок
