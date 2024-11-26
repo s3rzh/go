6603b1bb9eafa4ed0baa7f6919ab75f1f25ce0ff
s3rzh <serzh.vs@gmail.com>
2024-11-26 13:27:28 +0300

    Create main.go

---
+++ common_tasks/01_string/06_palindrome/1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(IsPalindrome("Hello")) // false
+	fmt.Println(IsPalindrome("oko"))   // true
+}
+
+func IsPalindrome(str string) bool {
+	for i := 0; i < len(str)/2; i++ {
+		if str[i] != str[len(str)-i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Палиндром — слово, предложение или последовательность символов, которая абсолютно одинаково читается как в привычном направлении, так и в обратном. К примеру, “Anna” — это палиндром, а “table” и “John” — нет.
