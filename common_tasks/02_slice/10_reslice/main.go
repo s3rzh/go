6d40467fe3eb2df8a90d6b0c02a2091aa8de14b2
s3rzh <serzh.vs@gmail.com>
2024-11-27 19:13:10 +0300

    Update main.go

--- common_tasks/02_slice/10_reslice/main.go
+++ common_tasks/02_slice/10_reslice/main.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	test1 := []int{1, 2, 3, 4, 5}
 	test1 = test1[:3]  // [1 2 3] 3 5
-	test2 := test1[3:] //test2 [] пусто и len=0 cap=2
+	test2 := test1[3:] //test2: [] пусто и len=0 cap=2
 	fmt.Println(test2)
 
 	// но тоже самое, реслайсинг с указаниме 5
-	test3 := test1[3:5] //test3= [4 5] 2 2
+	test3 := test1[3:5] //test3: [4 5] и len=2 cap=2
 	fmt.Println(test3)
 }
 
