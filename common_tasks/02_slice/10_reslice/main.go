d63756bf503e74e814ade1490139c266b84d7bdc
s3rzh <serzh.vs@gmail.com>
2024-11-27 19:12:34 +0300

    Create main.go

---
+++ common_tasks/02_slice/10_reslice/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	test1 := []int{1, 2, 3, 4, 5}
+	test1 = test1[:3]  // [1 2 3] 3 5
+	test2 := test1[3:] //test2 [] пусто и len=0 cap=2
+	fmt.Println(test2)
+
+	// но тоже самое, реслайсинг с указаниме 5
+	test3 := test1[3:5] //test3= [4 5] 2 2
+	fmt.Println(test3)
+}
+
+// Найти почему есть разница между test2 и test3?
