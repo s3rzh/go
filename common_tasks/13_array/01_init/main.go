88ae6fe47f75aec9df13dd64969b3238c74e09c1
s3rzh <serzh.vs@gmail.com>
2024-12-04 16:56:46 +0300

    Update main.go

--- common_tasks/13_array/01_init/main.go
+++ common_tasks/13_array/01_init/main.go
@@ -1,1 +1,10 @@
-// TODO
+package main
+
+func main() {
+	var data1 [5]int               // [0 0 0 0 0]
+	var data2 [2][5]int            // [[0 0 0 0 0] [0 0 0 0 0]]
+	data3 := [...]int{1, 2, 3}     // [1 2 3]
+	data4 := [5]int{1, 2, 3}       // [1 2 3 0 0]
+	data5 := [5]int{3: 4}          // [0 0 0 4 0]
+	data6 := [5]int{2: 5, 6, 1: 7} // [0 7 5 6 0]
+}
